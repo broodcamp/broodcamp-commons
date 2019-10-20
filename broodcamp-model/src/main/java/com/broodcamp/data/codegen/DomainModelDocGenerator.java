98457a13d8a3ffacd694bfeff584f044123c5374
Edward P. Legaspi <czetsuya@gmail.com>
2019-10-20 18:44:06 +0800

    Update address entities.

--- broodcamp-model/src/main/java/com/broodcamp/data/codegen/DomainModelDocGenerator.java
+++ broodcamp-model/src/main/java/com/broodcamp/data/codegen/DomainModelDocGenerator.java
@@ -1,3 +1,20 @@
+/**
+ * Broodcamp Library
+ * Copyright (C) 2019 Edward P. Legaspi (https://github.com/czetsuya)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.broodcamp.data.codegen;
 
 import java.io.BufferedWriter;
@@ -70,8 +87,6 @@ public class DomainModelDocGenerator {
 
         String modelDir = args[0];
         String outputDir = args[1];
-
-        System.out.println("Will parse " + modelDir + " and write db documentation to " + outputDir);
 
         TypeSolver reflectionTypeSolver = new ReflectionTypeSolver();
         TypeSolver javaParserTypeSolver = new JavaParserTypeSolver(new File(modelDir));
@@ -860,6 +875,7 @@ public class DomainModelDocGenerator {
             return fieldname;
         }
 
+        @SuppressWarnings("unused")
         public String getDbFieldname() {
             return dbFieldname;
         }
--- broodcamp-model/src/main/java/com/broodcamp/data/entity/adm/Region.java
+++ broodcamp-model/src/main/java/com/broodcamp/data/entity/adm/Region.java
@@ -1,3 +1,20 @@
+/**
+ * Broodcamp Library
+ * Copyright (C) 2019 Edward P. Legaspi (https://github.com/czetsuya)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.broodcamp.data.entity.adm;
 
 import javax.persistence.Cacheable;
@@ -16,23 +33,22 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 /**
- * @author Edward P. Legaspi
- * @created 14 Jan 2018
+ * @author Edward P. Legaspi | czetsuya@gmail.com
  **/
 @Entity
 @Cacheable
 @Table(name = "adm_region", uniqueConstraints = @UniqueConstraint(columnNames = { "country_id", "name" }))
 @Data
-@EqualsAndHashCode(callSuper = false, of = { "country", "name" })
+@EqualsAndHashCode(callSuper = false, of = { "country" })
 @AllArgsConstructor
 @NoArgsConstructor
 //@ApiModel
 public class Region extends NamedEntity {
 
-	private static final long serialVersionUID = -5624009746303674978L;
+    private static final long serialVersionUID = -5624009746303674978L;
 
-	@ManyToOne(optional = false, fetch = FetchType.LAZY)
-	@JoinColumn(name = "country_id", nullable = false)
-	private Country country;
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id", nullable = false)
+    private Country country;
 
 }
